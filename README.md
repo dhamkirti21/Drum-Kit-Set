fca28dd02ca5b66cbdef9024c3cbb738e1468545
dhamkirti21 <78336507+dhamkirti21@users.noreply.github.com>
2022-08-07 20:22:46 +0530

    First Commit

--- README.md
+++ README.md
@@ -1,9 +1,1 @@
-
-
-  Introduction
-- This Project can be Used to make beautiful music
-
-Languages Used
-- HTML
-- CSS
-- JS
+# Drum-Kit-
---
+++ index.js
@@ -0,0 +1,44 @@
+var list_button = document.querySelectorAll("button")
+for(i=0;i<list_button.length;i++){
+    list_button[i].addEventListener("click",function(){
+        var button =  this.innerText;
+        buttonAnimation(button);
+        if(button==='w'){
+            var audio = new Audio("/sounds/crash.mp3");
+            audio.play();
+        }
+        else if(button==='a'){
+            var audio = new Audio("/sounds/kick-bass.mp3");
+            audio.play();
+        }
+        else if(button==='s'){
+            var audio = new Audio("/sounds/snare.mp3");
+            audio.play();
+        }
+        else if(button==='d'){
+            var audio = new Audio("/sounds/tom-1.mp3");
+            audio.play();
+        }
+        else if(button==='j'){
+            var audio = new Audio("/sounds/tom-2.mp3");
+            audio.play();
+        }
+        else if(button==='k'){
+            var audio = new Audio("/sounds/tom-3.mp3");
+            audio.play();
+        }
+        else {
+            var audio = new Audio("/sounds/tom-4.mp3");
+            audio.play();
+        }
+    });
+
+}
+
+function buttonAnimation(currentkey){
+    var pressed = document.querySelector("." + currentkey);
+    pressed.classList.add("pressed");
+    setTimeout(function(){
+       pressed.classList.remove("pressed")
+    },2000)
+}
