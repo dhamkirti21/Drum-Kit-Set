d296f8e87f88b132184ea762a83c94981d7a6597
dhamkirti21 <78336507+dhamkirti21@users.noreply.github.com>
2023-03-04 11:47:56 +0530

    Update index1.js

--- index1.js
+++ index1.js
@@ -1,36 +1,4 @@
-document.addEventListener('keydown', (event) => {
-    var button = event.key;
-    buttonAnimation(button);
-    
-    if(button==='w'){
-        var audio = new Audio("/sounds/crash.mp3");
-        audio.play();
-    }
-    else if(button==='a'){
-        var audio = new Audio("/sounds/kick-bass.mp3");
-        audio.play();
-    }
-    else if(button==='s'){
-        var audio = new Audio("/sounds/snare.mp3");
-        audio.play();
-    }
-    else if(button==='d'){
-        var audio = new Audio("/sounds/tom-1.mp3");
-        audio.play();
-    }
-    else if(button==='j'){
-        var audio = new Audio("/sounds/tom-2.mp3");
-        audio.play();
-    }
-    else if(button==='k'){
-        var audio = new Audio("/sounds/tom-3.mp3");
-        audio.play();
-    }
-    else {
-        var audio = new Audio("/sounds/tom-4.mp3");
-        audio.play();
-    }
-},false);
+
 
 var list_button = document.querySelectorAll("button")
 for(i=0;i<list_button.length;i++){
@@ -38,19 +6,19 @@ for(i=0;i<list_button.length;i++){
         var button =  this.innerText;
         buttonAnimation(button);
         if(button==='w'){
-            var audio = new Audio("/sounds/crash.mp3");
+            var audio = new Audio("sounds/crash.mp3");
             audio.play();
         }
         else if(button==='a'){
-            var audio = new Audio("/sounds/kick-bass.mp3");
+            var audio = new Audio("sounds/kick-bass.mp3");
             audio.play();
         }
         else if(button==='s'){
-            var audio = new Audio("/sounds/snare.mp3");
+            var audio = new Audio("sounds/snare.mp3");
             audio.play();
         }
         else if(button==='d'){
-            var audio = new Audio("/sounds/tom-1.mp3");
+            var audio = new Audio("sounds/tom-1.mp3");
             audio.play();
         }
         else if(button==='j'){
